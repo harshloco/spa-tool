f941d5a5bebd822de422ca1501e0d425ad1eb446
abhinavgpillai123 <abhinavgpillai123@gmail.com>
2021-12-17 14:25:06 +1100

    UI CHanges3

--- resources/views/report.blade.php
+++ resources/views/report.blade.php
@@ -10,44 +10,127 @@
     <title>Chartisan example</title>
 </head>
 <body>
-<!-- Chart's container -->
-<div id="chart" style="height: 300px;"></div>
-<div class="table-responsive">
-    <!-- like table -->
-    <table class="table align-items-center table-flush">
-        <thead class="thead-light">
-        <tr>
-            <th scope="col">User Name</th>
-            <th scope="col">Post Url</th>
-            <th scope="col">Likes Count</th>
-            <th scope="col">Comments Count</th>
-        </tr>
-        </thead>
-        <tbody>
-        @foreach($mostLiked as $liked)
-            <tr>
-                <th scope="row">
-                    {{$liked->name}}
-                </th>
-                <td>
-                    <a target="blank" href ={{$liked->postUrl}}>Link</a>
-                </td>
-                <td>
-                    {{$liked->likeCount}}
-                </td>
-                <td>
-                    {{$liked->commentCount}}
-                </td>
-            </tr>
-        @endforeach
+<div class="container-fluid mt--7">
+    <div class="row mt-5">
+        <div class="col-xl-12 mb-5 mb-xl-10">
+            <div class="card shadow">
+                <div class="card-header border-0">
+                    <div class="row align-items-center">
+                        <div class="col">
+                            <h3 class="mb-0">Details</h3>
+                        </div>
+                        <div class="col text-right">
+                            <a href="#!" class="btn btn-sm btn-primary">See all</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="table-responsive">
+                    <!-- like table -->
+                    <table class="table align-items-center table-flush">
+                        <thead class="thead-light">
+                        <tr>
+                            <th scope="col">User Name</th>
+                            <th scope="col">Post Url</th>
+                            <th scope="col">Likes Count</th>
+                            <th scope="col">Comments Count</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach($mostLiked as $liked)
+                            <tr>
+                                <th scope="row">
+                                    {{$liked->name}}
+                                </th>
+                                <td>
+                                    <a target="blank" href ={{$liked->postUrl}}>Link</a>
+                                </td>
+                                <td>
+                                    {{$liked->likeCount}}
+                                </td>
+                                <td>
+                                    {{$liked->commentCount}}
+                                </td>
+                            </tr>
+                        @endforeach
 
-        </tbody>
-    </table>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-xl-12 mb-5 mb-xl-10">
+            <div class="card shadow">
+                <div class="card-header border-0">
+                    <div class="row align-items-center">
+                        <div class="col">
+                            <h3 class="mb-0">Top 4 most liked post with #{{ $data }}</h3>
+                        </div>
+                        <div class="col text-right">
+                            <a href="#!" class="btn btn-sm btn-primary">See all</a>
+                        </div>
+                    </div>
+                </div>
+                <!-- Chart's container -->
+                <div id="chart" style="height: 300px;"></div>
+            </div>
+        </div>
+        <div class="col-xl-12">
+            <div class="card shadow">
+                <div class="card-header border-0">
+                    <div class="row align-items-center">
+                        <div class="col">
+                            <h3 class="mb-0">Top 4 most commented post with #{{ $data }}</h3>
+                        </div>
+                        <div class="col text-right">
+                            <a href="#!" class="btn btn-sm btn-primary">See all</a>
+                        </div>
+                    </div>
+                </div>
+                <!-- Chart's container -->
+                <div id="comment" style="height: 300px;"></div>
+            </div>
+        </div>
+    </div>
+
 </div>
-<div id="comment" style="height: 300px;"></div>
+
+    <!--<div class="table-responsive">
+        &lt;!&ndash; like table &ndash;&gt;
+        <table class="table align-items-center table-flush">
+            <thead class="thead-light">
+            <tr>
+                <th scope="col">User Name</th>
+                <th scope="col">Post Url</th>
+                <th scope="col">Likes Count</th>
+                <th scope="col">Comments Count</th>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach($mostLiked as $liked)
+                <tr>
+                    <th scope="row">
+                        {{$liked->name}}
+                    </th>
+                    <td>
+                        <a target="blank" href ={{$liked->postUrl}}>Link</a>
+                    </td>
+                    <td>
+                        {{$liked->likeCount}}
+                    </td>
+                    <td>
+                        {{$liked->commentCount}}
+                    </td>
+                </tr>
+            @endforeach
+
+            </tbody>
+        </table>
+    </div>-->
+
 
 <!-- comment table -->
-<table class="table align-items-center table-flush ml--2">
+<!--<table class="table align-items-center table-flush ml&#45;&#45;2">
     <thead class="thead-light">
     <tr>
         <th scope="col">User Name</th>
@@ -75,7 +158,7 @@
     @endforeach
 
     </tbody>
-</table>
+</table>-->
 
 <!-- Charting library -->
 <script src="https://unpkg.com/chart.js@^2.9.3/dist/Chart.min.js"></script>
@@ -92,7 +175,6 @@
             .responsive()
             .beginAtZero()
             .legend({ position: 'bottom' })
-            .title('Top 4 most liked post with #{{ $data }}')
     });
 
     const comment = new Chartisan({
@@ -103,7 +185,6 @@
             .responsive()
             .beginAtZero()
             .legend({ position: 'bottom' })
-            .title('Top 4 most commented post with #{{ $data }}')
     });
 </script>
 </body>
